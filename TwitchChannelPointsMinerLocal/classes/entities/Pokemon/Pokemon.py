18575d6716abe42913a147e8c331b2fdef6f554b
Dominic-Santos <dominic.ah.santos@gmail.com>
2023-03-01 11:08:40 +0000

    pokemon id in print

--- TwitchChannelPointsMinerLocal/classes/entities/Pokemon/Pokemon.py
+++ TwitchChannelPointsMinerLocal/classes/entities/Pokemon/Pokemon.py
@@ -23,5 +23,5 @@ class Pokemon(object):
             self.name = self.name.replace(" ", " (") + ")"
 
     def __str__(self):
-        alt_name = "" if self.is_alternate is False else f" ({self.alt_name})"
+        alt_name = "" if self.is_alternate is False else f" ({self.pokemon_id} - {self.alt_name})"
         return f"{self.spawn} {self.name}{alt_name}, {self.bst}BST, {self.weight}KG, tier {self.tier}, types {self.types}"
