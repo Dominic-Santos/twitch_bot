e9c87227b17ef83167a637a9d9839eda4e5f77ef
Dominic-Santos <dominic.ah.santos@gmail.com>
2023-06-11 22:36:38 +0100

    removing print and tiers

--- TwitchChannelPointsMinerLocal/classes/entities/Pokemon/Missions.py
+++ TwitchChannelPointsMinerLocal/classes/entities/Pokemon/Missions.py
@@ -76,7 +76,7 @@ class Missions(object):
                 mission_title = mission["name"].lower().replace("[flash]", " ").replace("[event]", " ").replace("é", "e").replace("wonder trade", "wondertrade").strip()
                 mission_title = "".join([c for c in mission_title if c.isalnum() or c == " "]).strip()
                 mission_title = " ".join([w for w in mission_title.split(" ") if w != ""])
-                print(mission_title)
+
                 if mission_title.startswith("wondertrade"):
                     if mission_title == "wondertrade":
                         # just wondertrade anything does not require a mission
